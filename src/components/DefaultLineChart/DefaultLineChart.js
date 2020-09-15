d7901ab12ed6bf1fb10bc9fc408dad95424ced1c
mahsa-nvr <mahsaanvari2@gmail.com>
2020-09-15 16:58:51 +0430

    app linechart

---
+++ src/components/DefaultLineChart/DefaultLineChart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DefaultLineChart extends React.Component { 
+    render() {
+        return(
+            <div>rfrfrfr</div>
+        )
+    }
+}
+
+export default DefaultLineChart;
