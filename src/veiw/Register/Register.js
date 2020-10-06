fa37dab6fe191019b4297f873dd2e8053141c917
mahsa-nvr <mahsaanvari2@gmail.com>
2020-10-06 16:43:35 +0330

    add first login

--- src/veiw/Register/Register.js
+++ src/veiw/Register/Register.js
@@ -6,10 +6,13 @@ import daryan from './../../assets/img/daryan.png';
 import { withRouter } from "react-router-dom";
 
 class Register extends React.Component {
+    
 
     taeed= (props) => {
         this.props.history.push('Defaultpage')
     }
+
+
     render() {
         return (
             <div className="main_Reg"> 
@@ -20,11 +23,15 @@ class Register extends React.Component {
                 </div>
 
                 <div className="first_input d-flex justify-content-center ">
-                    <Input placeholder="شماره موبایل" style={{width: "80%" }} />
+                    <Input placeholder="شماره موبایل" style={{width: "80%", fontSize: "12px"}} />
                 </div>
 
                 <div  className="first_btn d-flex justify-content-center ">
-                    <Button onClick={this.taeed} color="success" style={{ width: "50%", backgroundColor: "#20bf6b"}}>تایید</Button>
+                    <Button onClick={this.taeed} color="success" style={{ width: "50%", backgroundColor: "#20bf6b", fontSize:"15px"}}>تایید</Button>
+                </div>
+
+                <div  className="second_btn d-flex justify-content-center ">
+                    <Button onClick={this.taeed}  style={{ width: "50%", backgroundColor: "white", border: "1px solid #07b0c3" , color:"#07b0c3", fontSize:"12px"}}>تخلیه اطلاعات</Button>
                 </div>
   
             </div>
