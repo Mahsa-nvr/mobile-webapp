63a3488b11937c336ea469e6a7fc9e2e15224f28
Mahsa-nvr <mahsaanvari2@gmail.com>
2020-10-11 16:34:04 +0330

    some changes in spend page

--- src/components/SpendListItem/SpendList.js
+++ src/components/SpendListItem/SpendList.js
@@ -1,13 +1,147 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import {  ListGroupItem, Button , Row, Col, Form, FormGroup, Label, Input} from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
+import axios from 'axios';
+import {API} from './../../Services/Config';
+import { HandleChange , handlePriceChange } from './../../share/Utility';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+import './SpendList.css'
 
 class SpendList extends React.Component {
+
+    constructor(props){
+        super();
+        this.state = {
+            inputName: "",
+            inputAmount: "",
+            inputDate: '',
+            totalSpend: [],
+            show: true
+        }
+    }
+
+    componentDidMount(){
+        axios.get(`${API}expenditures/index`, {
+            params: {
+                user_id : 1         
+            }
+           }
+          ).then(res => {
+          this.setState({
+            totalSpend :[...res.data]
+        })
+        
+          }).catch(err => {
+            console.log(err)
+          })
+    }
+
+    btnClick = () => {
+        this.setState({ 
+          show : !this.state.show
+        });
+    }
+
     render() {
+      
         return (
             <div>
-                <ListGroup>
-        <ListGroupItem>{this.props.mainTitle}</ListGroupItem>
-                </ListGroup>
+           
+                  <ListGroupItem className="spend_list_group header">
+                      <div className="asset_list_img"><FontAwesomeIcon icon={faBars} /></div>
+                      
+                      {this.props.mainTitle}
+                      
+                      <div className="base_list_btn">
+                        <Button onClick={this.btnClick} className="list_btn">+</Button>
+                      </div>
+
+                 </ListGroupItem>
+
+                 {this.state.show ? 
+                  <div className="list_base_show">
+                          
+                  <Row >
+                      <Col>
+                         
+                        <FormGroup className="form_base_part">
+                          <AvForm  onChange={(e) => HandleChange.call(this, e)} >
+                            <AvField 
+                             name="inputName"
+                             label="نام" 
+                             type="text" 
+                             value={this.state.inputName}                                         
+                             errorMessage="نام را وارد کنید" 
+                             validate={{
+                                        required: {value: true},
+                                        pattern: {value: '^[A-Za-z0-9پچجحخهعغفقثصضشسیبلاتنمکگوئدذرزطظژؤإأءًٌٍَُِّs]+$'}, 
+                                      }} />
+                          </AvForm>
+                        </FormGroup>
+                          
+                    </Col>
+                    <Col>
+                     
+                        <FormGroup className="form_base_part">
+                          <AvForm onChange={(e) => handlePriceChange.call(this, e)}>
+                          
+                            <AvField 
+                             name="inputAmount"
+                             label="قیمت" 
+                             type="text" 
+                             value={this.state.inputAmount}                                            
+                             errorMessage="قیمت را وارد کنید" 
+                             validate={{
+                                        number: true,
+                                        required: {value: true, errorMessage:"قیمت را وارد کنید"},
+                                        pattern: {value: '^[0-9]+$'},                                                
+                                      }} />
+                            
+                   
+                          </AvForm>                                   
+                             </FormGroup>
+                           
+                      </Col>
+                  </Row>
+                  <Row>
+                      <Col>
+                           <Form>
+                             <FormGroup className="form_base_part">
+                               <Label for="">تاریخ</Label>
+                               <div className="date_picker">
+                                  
+                             
+                               </div>
+                               <Input type="date" 
+                                      bsSize="sm" 
+                                      name="inputDate" 
+                                      value={this.state.inputDate} 
+                                      onChange={(e) => HandleChange.call(this, e)}  
+                                      />
+                             </FormGroup>
+                           </Form>
+                      </Col>
+                      <Col>
+                        <div className="form_part_send_btn">
+                        <Button className="send_btn" color="primary" onClick={this.send}> ارسال</Button>
+                        </div>
+                        </Col>
+                  </Row> 
+                
+              </div>
+                 
+                 : null }
+
+                 {this.state.totalSpend.map(li => {
+                     if(parseInt(li.category_id) === this.props.catId) 
+                     return <ListGroupItem  key={li.id} className="spend_list_part">
+                         {li.name}
+                         <span className="asset_list_part_amount">{li.amount}</span>
+                         </ListGroupItem>
+                 }  )}
+        
 
             </div>
         )
