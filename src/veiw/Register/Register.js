c9459d2bdb9674e1719b94f26eded028348ed6a1
mahsa-nvr <mahsaanvari2@gmail.com>
2020-10-18 16:47:05 +0330

    start register

--- src/veiw/Register/Register.js
+++ src/veiw/Register/Register.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import './Register.css';
 import {Input , Button} from 'reactstrap';
+import NumberFormat from 'react-number-format';
+
 // import baseReg from './../../assets/img/baseReg.png';
 import daryan from './../../assets/img/daryan.png';
+import center from './../../assets/img/daryan.png'
 import { withRouter } from "react-router-dom";
 
 class Register extends React.Component {
-    
+
+    constructor(props){
+        super();
+        this.state={
+            inputPhone:''
+        }
+    }
+
+    handleChange=(event) => {
+        const inputPhone = event.target.value
+        inputPhone.split()
+        this.setState({ test:inputPhone})
+        }
 
     taeed= (props) => {
+       
         this.props.history.push('Defaultpage')
+   
     }
 
 
     render() {
+
         return (
             <div className="main_Reg"> 
                 <div className="register_first d-flex justify-content-center"> 
+               
                   <div className="register_second d-flex mt-auto">
                       <img className="reg_sec_img" height={130} src={daryan} alt=""/>
                   </div> 
                 </div>
 
                 <div className="first_input d-flex justify-content-center ">
-                    <Input placeholder="شماره موبایل" style={{width: "80%", fontSize: "12px"}} />
+                <NumberFormat  dir="ltr" value={this.state.test} onChange={this.handleChange} style={{borderRadius:'5%',borderColor:"grey"}} displayType="input" format="09#########" allowEmptyFormatting mask="*"/>
                 </div>
 
                 <div  className="first_btn d-flex justify-content-center ">
