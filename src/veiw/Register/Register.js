ffaabb4f53d7ffb272c4512faa231d0aef787165
Mahsa-nvr <mahsaanvari2@gmail.com>
2020-10-19 11:19:47 +0330

    register page

--- src/veiw/Register/Register.js
+++ src/veiw/Register/Register.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import axios from 'axios';
+import  { API } from './../../Services/Config.js';
 import './Register.css';
-import {Input , Button} from 'reactstrap';
+import { Button} from 'reactstrap';
 import NumberFormat from 'react-number-format';
 
-// import baseReg from './../../assets/img/baseReg.png';
+
 import daryan from './../../assets/img/daryan.png';
-import center from './../../assets/img/daryan.png'
+// import center from './../../assets/img/daryan.png'
 import { withRouter } from "react-router-dom";
 
 class Register extends React.Component {
@@ -13,25 +15,55 @@ class Register extends React.Component {
     constructor(props){
         super();
         this.state={
-            inputPhone:''
+            inputPhone:'',
+            empty: false
         }
     }
 
     handleChange=(event) => {
-        const inputPhone = event.target.value
-        inputPhone.split()
-        this.setState({ test:inputPhone})
+        const {name, value } = event.target;
+        this.setState({
+            [name] : value,
+            empty: false
+        })
         }
 
     taeed= (props) => {
-       
-        this.props.history.push('Defaultpage')
-   
+        if(this.state.inputPhone) {
+        const test = this.state.inputPhone
+        var arr=test.split('');
+        var arr2= arr.includes('*')
+        if (!arr2 ) {
+            localStorage.setItem("inputPhone", this.state.inputPhone )
+            this.props.history.push('Defaultpage')
+        }else{
+            this.setState({
+                empty: true
+            })
+        }
+
+    }else {
+        this.setState({
+            empty: true
+        })
+    }
     }
 
+    register = () => {
+        axios.get(`${API}database/clean`,{
+            params: {
+              user_id : 1,
+              clean: true
+          }
+          }).then(res => {
+              console.log(res)
+            console.log('empty data base', res)
+          }).catch(err =>
+             console.log('err to empty data base' , err))
+
+    }
 
     render() {
-
         return (
             <div className="main_Reg"> 
                 <div className="register_first d-flex justify-content-center"> 
@@ -41,8 +73,9 @@ class Register extends React.Component {
                   </div> 
                 </div>
 
-                <div className="first_input d-flex justify-content-center ">
-                <NumberFormat  dir="ltr" value={this.state.test} onChange={this.handleChange} style={{borderRadius:'5%',borderColor:"grey"}} displayType="input" format="09#########" allowEmptyFormatting mask="*"/>
+               
+                <div className="first_input d-flex justify-content-center ">                   
+                <NumberFormat className={this.state.empty?'empty_text': 'full_text'} dir="ltr" name="inputPhone" value={this.state.inputPhone} onChange={this.handleChange}  displayType="input" format="09#########" allowEmptyFormatting mask="*"/>
                 </div>
 
                 <div  className="first_btn d-flex justify-content-center ">
@@ -50,7 +83,7 @@ class Register extends React.Component {
                 </div>
 
                 <div  className="second_btn d-flex justify-content-center ">
-                    <Button onClick={this.taeed}  style={{ width: "50%", backgroundColor: "white", border: "1px solid #07b0c3" , color:"#07b0c3", fontSize:"12px"}}>تخلیه اطلاعات</Button>
+                    <Button onClick={this.register}  style={{ width: "50%", backgroundColor: "white", border: "1px solid #07b0c3" , color:"#07b0c3", fontSize:"12px"}}> ثبت نام</Button>
                 </div>
   
             </div>
