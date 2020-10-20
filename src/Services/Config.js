cdf7cb0f066ab09ad0cada22e532144f558fd719
Mahsa-nvr <mahsaanvari2@gmail.com>
2020-10-20 10:54:26 +0330

    apii

--- src/Services/Config.js
+++ src/Services/Config.js
@@ -1,3 +1,3 @@
 // export const API="http://192.168.22.48/ipfm/frontend/web/index.php/";
 
-export const API="https://ipfm.sefroweb.com/frontend/web/index.php/";
+export const API="http://ipfm.sefroweb.com/frontend/web/index.php/";
