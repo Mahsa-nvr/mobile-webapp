9a5896aaa7235c2e111dd0090622c6bc11fbeda1
Mahsa-nvr <mahsaanvari2@gmail.com>
2020-11-11 15:19:24 +0330

    addddd

---
+++ src/veiw/Mainprofile/Mainprofile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Mainprofile extends React.Component {
+    render() {
+        return (
+            <div>profile eeee</div>
+        )
+    }
+}
+
+export default Mainprofile;
